d1ed666d7cd263e51fa3f948f0d06d6dcd56a576
joao0 <joao63430@gmail.com>
2022-09-27 12:18:07 +0200

    hello world

---
+++ main.py
@@ -0,0 +1,19 @@
+class People:
+    name: str = ""
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Intro:
+    hello: str = "Hello"
+
+    people: People = People("world")
+
+    def print_hello(self, name):
+        print(f'{self.hello} {self.people.name}, Bonne journée {name}.')
+
+
+intro: Intro = Intro()
+
+intro.print_hello('Joao')
